00be9329eaebe73a7ce106b7cf29a24dc7326324
Koju Sakaguchi <fuetf@icloud.com>
2021-12-12 14:09:51 +0900

    create form

--- src/App.js
+++ src/App.js
@@ -3,7 +3,32 @@ import React from 'react';
 const App = () => {
   return (
     <div>
-      Hi, there!
+      <h1>TODO LIST</h1>
+
+      <label htmlFor="title">TITLE</label>
+      <input id='title'></input>
+
+      <select value=''>
+        <option value=''>Please Choose Option</option>
+        <option value='notStarted'>notStarted</option>
+        <option value='inProgress'>inProgress</option>
+        <option value='done'>done</option>
+      </select>
+
+      <button>ADD</button>
+
+      <table>
+        <tr>
+          <th>ID</th>
+          <th>TITLE</th>
+          <th>COMPLETE</th>
+        </tr>
+        <tr>
+          <td></td>
+          <td>Francisco</td>
+          <td>done</td>
+        </tr>
+      </table>
     </div>
   )
 
