dbb9abcc8b51dd3c5b97f086f4e59e1e82ebba50
Koju Sakaguchi <fuetf@icloud.com>
2021-12-12 14:22:24 +0900

    utilize useState

--- src/App.js
+++ src/App.js
@@ -1,37 +1,38 @@
-import React from 'react';
+import { useState } from "react";
 
 const App = () => {
+  const [title, setTitle] = useState("");
+  const [complete, setComplete] = useState("");
+
   return (
     <div>
       <h1>TODO LIST</h1>
 
-      <label htmlFor="title">TITLE</label>
-      <input id='title'></input>
+      <label htmlFor="title">TITLE:</label>
+      <input
+        id="title"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+      ></input>
 
-      <select value=''>
-        <option value=''>Please Choose Option</option>
-        <option value='notStarted'>notStarted</option>
-        <option value='inProgress'>inProgress</option>
-        <option value='done'>done</option>
+      <select value={complete} onChange={(e) => setComplete(e.target.value)}>
+        <option value="">Please Choose Option</option>
+        <option value="notStarted">notStarted</option>
+        <option value="inProgress">inProgress</option>
+        <option value="done">done</option>
       </select>
 
       <button>ADD</button>
 
       <table>
-        <tr>
+        {/* <tr>
           <th>ID</th>
           <th>TITLE</th>
           <th>COMPLETE</th>
-        </tr>
-        <tr>
-          <td></td>
-          <td>Francisco</td>
-          <td>done</td>
-        </tr>
+        </tr> */}
       </table>
     </div>
-  )
-
-}
+  );
+};
 
 export default App;
